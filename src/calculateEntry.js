024bf2dad52bd681b53db406fd8c6bf0d100b88b
BradockBr <robtognetti@gmail.com>
2022-08-02 18:40:04 -0300

    calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((allVisitors, visitor) => {
+    const totalVisitors = allVisitors;
+    if (visitor.age < 18) {
+      totalVisitors.child += 1;
+    } else if (visitor.age >= 18 && visitor.age < 50) {
+      totalVisitors.adult += 1;
+    } else {
+      totalVisitors.senior += 1;
+    }
+    return allVisitors;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { adult, senior, child } = prices;
+  const t = countEntrants(entrants);
+  return (t.child * child) + (t.adult * adult) + (t.senior * senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
