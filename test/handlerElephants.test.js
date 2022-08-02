77a5429c01379ea3b27950d4cb683148c8d8c96d
BradockBr <robtognetti@gmail.com>
2022-08-02 19:30:08 -0300

    handlerElephants

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,15 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Informações referentes aos Elephants', () => {
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('averageAge')).toBe(10.5);
+    expect(handlerElephants('location')).toBe('NW');
+    expect(handlerElephants('popularity')).toBe(5);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+    expect(handlerElephants()).toBeUndefined();
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants(' ')).toBe(null);
+  });
+});
