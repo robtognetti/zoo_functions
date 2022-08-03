1a81ba402d3aad767792791c7d1f0f82690e84c9
BradockBr <robtognetti@gmail.com>
2022-08-03 12:31:59 -0300

    getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (!ids) {
+    return [];
+  }
+  const animais = species.filter((specie) => ids.includes(specie.id));
+  return animais;
 }
 
 module.exports = getSpeciesByIds;
