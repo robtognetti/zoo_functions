ffe43ea5dc1687bc080249ee9900cc584e2a2fe4
BradockBr <robtognetti@gmail.com>
2022-08-03 01:36:24 -0300

    getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.find((item) => id === item.id).managers
+    .every((item) => item.includes('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return data.employees.filter((item) => item.managers.includes(managerId))
+      .map((item) => `${item.firstName} ${item.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
