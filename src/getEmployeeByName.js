3b04a1c15bf2b601beb37179c96777fafbfdd0c8
BradockBr <robtognetti@gmail.com>
2022-08-03 00:05:24 -0300

    getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(employeedName) {
+  if (employeedName === undefined) {
+    return {};
+  }
+  return employees.find((employee) => employeedName === employee.firstName
+  || employeedName === employee.lastName);
 }
 
 module.exports = getEmployeeByName;
