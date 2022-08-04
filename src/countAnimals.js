281613a00dd4be4669bc3fb34d5479e265afe831
BradockBr <robtognetti@gmail.com>
2022-08-03 23:04:56 -0300

    countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = (animal) => {
+  if (!animal) {
+    return species
+      .reduce((acc, curr) => {
+        acc[curr.name] = curr.residents.length;
+        return acc;
+      }, {});
+  }
+  const animais = species
+    .find((animals) => animals.name === Object.values(animal)[0]);
+  if (!animal.sex) {
+    return animais.residents.length;
+  } if (animal.sex === 'female') {
+    return animais.residents.filter((animals) => animals.sex === 'female').length;
+  } if (animal.sex === 'male') {
+    return animais.residents.filter((animals) => animals.sex === 'male').length;
+  }
+};
 
 module.exports = countAnimals;
