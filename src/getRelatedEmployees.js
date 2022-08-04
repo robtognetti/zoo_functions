e2e759f1a52ddbf32797be450608839f3b4c7a33
BradockBr <robtognetti@gmail.com>
2022-08-04 12:07:46 -0300

    getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,7 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return data.employees.find((item) => id === item.id).managers
-    .every((item) => item.includes('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+  return data.employees.some((item) => item.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
@@ -13,5 +12,6 @@ function getRelatedEmployees(managerId) {
       .map((item) => `${item.firstName} ${item.lastName}`);
   }
 }
-
+/* return data.employees.find((item) => id === item.id).managers
+    .every((item) => item.includes('9e7d4524-363c-416a-8759-8aa7e50c0992')); */
 module.exports = { isManager, getRelatedEmployees };
