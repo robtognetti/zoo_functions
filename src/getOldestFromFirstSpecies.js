4e4fdef8b382b41ed348f616aaba9e0ae3adc02f
BradockBr <robtognetti@gmail.com>
2022-08-08 23:34:18 -0300

    getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-
+  const responsavel = data.employees.find((elemento) => elemento.id === id).responsibleFor[0];
+  const localizaAnimais = data.species.find((animal) => responsavel === animal.id).residents;
+  const idadeAnimais = localizaAnimais.reduce((acc, curr) => (acc.age > curr.age ? acc : curr), []);
+  return Object.values(idadeAnimais);
 }
 
 module.exports = getOldestFromFirstSpecies;
