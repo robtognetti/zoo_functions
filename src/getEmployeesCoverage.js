b1abb0a8d5c0ed0f85fbeb2c397de7d006d36e83
BradockBr <robtognetti@gmail.com>
2022-08-08 23:03:02 -0300

    getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,35 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+const array = employees.map((employee) => {
+  const objetoEmployee = {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: employee.responsibleFor
+      .map((animal) => species.filter((specie) => specie.id === animal)[0].name),
+    locations: employee.responsibleFor
+      .map((animal) => species.filter((specie) => specie.id === animal)[0].location),
+  };
+  return objetoEmployee;
+});
+
+function getEmployeesCoverage(elemento) {
+  if (!elemento) {
+    return array;
+  }
+  const entry = array.find((element) => element.fullName.includes(Object.values(elemento))
+  || element.id.includes(Object.values(elemento)));
+  if (employees.find((employee) => employee.firstName === employee.name)) {
+    return entry;
+  }
+  if (employees.find((employee) => employee.lastName === employee.name)) {
+    return entry;
+  }
+  if (!entry) {
+    throw new Error('Informações inválidas');
+  }
+  return entry;
 }
 
 module.exports = getEmployeesCoverage;
